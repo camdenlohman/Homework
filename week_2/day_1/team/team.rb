b03109a42c305dd24c95527fb83e07d97db140b1
Camden Lohman <camden.lohman1@gmail.com>
2016-11-28 17:05:37 +0000

    week2_day1

--- week_2/day_1/team/team.rb
+++ week_2/day_1/team/team.rb
@@ -8,35 +8,39 @@ class Team
     #instance variable / private variable
   end
 
-def get_team_name
-return @team_name
-end
-
-def get_players
-return @players
-end
-
-def get_coach
-return @coach
-end
-
-def set_coach(change)
-  @coach = change
-return @coach
-end
-
-def new_player(change)
-@players << change
+  def get_team_name
+    return @team_name
   end
 
-def update_score(score)
-if score == "2"
-  @score = "1"
-end
-if score == "1"
-  @score = "2"
-else
-  @score = "0"
-end
-end
+  def get_players
+    return @players
+  end
+
+  def get_coach
+    return @coach
+  end
+
+  def find_player(array,search)
+    @players.include? (search)
+  end
+
+  def set_coach(change)
+    @coach = change
+    return @coach
+  end
+
+  def new_player(change)
+    @players << change
+  end
+
+  def update_score(score)
+    if score == "2"
+      @score = "1"
+    end
+    if score == "1"
+      @score = "2"
+    else
+      @score = "0"
+    end
+  end
 end
--- week_2/day_1/team/team_spec.rb
+++ week_2/day_1/team/team_spec.rb
@@ -28,6 +28,11 @@ class TeamSpec < MiniTest::Test
     assert_equal(["player1","player2","player3","player4","player5"], team1.new_player("player5"))
   end
 
+  def test_find_player
+    team1 = Team.new("team",["player1","player2","player3","player4"],"coach")
+    assert_equal(true, team1.find_player(team1,"player4"))
+  end
+
   def test_update_score
     team1 = Team.new("team",["player1","player2","player3","player4"],"coach")
     assert_equal("2", team1.update_score("1"))
